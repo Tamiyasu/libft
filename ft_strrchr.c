fb9f179077c010e91c6d4235ca82dd76dde69fa1
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2020-06-23 21:15:39 +0900

    add ft_strrchr.c again

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,20 +6,20 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 14:45:39 by tmurakam          #+#    #+#             */
-/*   Updated: 2020/06/23 21:13:15 by tmurakam         ###   ########.fr       */
+/*   Updated: 2020/06/23 21:15:20 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *strrchr(const char *s, int c);
+char    *strrchr(const char *s, int c)
 {
     char *i_p;
 
-    i_p = s + ft_strlen(s);
+    i_p = (char *)s + ft_strlen(s);
     while (i_p != s || *i_p != c)
         i_p--;
     if (*i_p != c)
         i_p = 0;
     return (i_p);
-}v
+}
